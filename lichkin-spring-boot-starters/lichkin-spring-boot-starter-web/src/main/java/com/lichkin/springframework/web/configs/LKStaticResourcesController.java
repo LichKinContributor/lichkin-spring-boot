9c5c2def5026334b39d3a63a09ee3f18404c8ed5
Kin <zhuangxuxin@hotmail.com>
2018-04-21 13:49:41 +0800

    静态资源MD5访问辅助

---
+++ lichkin-spring-boot-starters/lichkin-spring-boot-starter-web/src/main/java/com/lichkin/springframework/web/configs/LKStaticResourcesController.java
@@ -0,0 +1,29 @@
+package com.lichkin.springframework.web.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.resource.ResourceUrlProvider;
+
+/**
+ * 静态资源文件请求处理
+ * @author SuZhou LichKin Information Technology Co., Ltd.
+ */
+@ControllerAdvice
+public class LKStaticResourcesController {
+
+	@Autowired
+	ResourceUrlProvider provider;
+
+
+	/**
+	 * 静态资源路径解析提供者
+	 * @param path 静态资源路径
+	 * @return 提供者
+	 */
+	@ModelAttribute("provider")
+	public ResourceUrlProvider provider() {
+		return provider;
+	}
+
+}
