c758abdd5d9344ab757c08d272ca2a02fe295061
Kin <zhuangxuxin@hotmail.com>
2018-05-16 02:20:18 +0800

    抽取API请求日志实体类

--- lichkin-spring-boot-entities/lichkin-spring-boot-entity-SysApiRequestLog/src/main/java/com/lichkin/springframework/entities/impl/SysApiRequestLogEntity.java
+++ lichkin-spring-boot-entities/lichkin-spring-boot-entity-SysApiRequestLog/src/main/java/com/lichkin/springframework/entities/impl/SysApiRequestLogEntity.java
@@ -1,15 +1,11 @@
 package com.lichkin.springframework.entities.impl;
 
-import java.util.Locale;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.validation.constraints.Pattern;
 
-import com.lichkin.framework.defines.enums.impl.LKClientTypeEnum;
-import com.lichkin.springframework.entities.suppers.LKMappedBaseSysEntity;
+import org.apache.commons.lang3.ArrayUtils;
+
+import com.lichkin.springframework.entities.suppers.LKMappedBaseSysApiRequestLogEntity;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -21,43 +17,10 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-public class SysApiRequestLogEntity extends LKMappedBaseSysEntity {
+public class SysApiRequestLogEntity extends LKMappedBaseSysApiRequestLogEntity {
 
 	/** serialVersionUID */
 	private static final long serialVersionUID = 8888886666666001L;
-
-	/**
-	 * 国际化
-	 * @see Locale
-	 */
-	@Pattern(regexp = "([A-Za-z]{2}){1}(_[A-Za-z]{2})?")
-	@Column(nullable = false, length = 5)
-	private String locale;
-
-	/** 客户端唯一标识 */
-	@Column(nullable = false, length = 128)
-	private String appKey;
-
-	/** 客户端类型 */
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false, length = 10)
-	private LKClientTypeEnum clientType;
-
-	/** 客户端版本号（大版本号） */
-	@Column(nullable = false)
-	private Byte versionX;
-
-	/** 客户端版本号（中版本号） */
-	@Column(nullable = false)
-	private Byte versionY;
-
-	/** 客户端版本号（小版本号） */
-	@Column(nullable = false)
-	private Short versionZ;
-
-	/** 登录后获取得 */
-	@Column(nullable = false, length = 64)
-	private String token = "";
 
 	/** 客户端系统版本 */
 	@Column(nullable = false, length = 16)
@@ -84,7 +47,7 @@ public class SysApiRequestLogEntity extends LKMappedBaseSysEntity {
 
 	@Override
 	protected Object[] getCheckCodeFieldValues() {
-		return new Object[] { locale, appKey, clientType, versionX, versionY, versionZ, token, osVersion, brand, model, uuid, screenWidth, screenHeight };
+		return ArrayUtils.addAll(super.getCheckCodeFieldValues(), osVersion, brand, model, uuid, screenWidth, screenHeight);
 	}
 
 }
