fe341949fe95f6ebd19b17d28e10661afe572190
Kin <zhuangxuxin@hotmail.com>
2018-05-28 15:11:08 +0800

    LKBeanUtils代码优化

--- lichkin-spring-boot-starter-base/src/main/java/com/lichkin/springframework/utils/LKBeanUtils.java
+++ lichkin-spring-boot-starter-base/src/main/java/com/lichkin/springframework/utils/LKBeanUtils.java
@@ -2,26 +2,11 @@ package com.lichkin.springframework.utils;
 
 import org.springframework.beans.BeanUtils;
 
-import com.lichkin.framework.defines.exceptions.LKFrameworkException;
-
 /**
  * Bean工具类
  * @author SuZhou LichKin Information Technology Co., Ltd.
  */
 public class LKBeanUtils {
-
-	/**
-	 * 复制属性
-	 * @param <B> 返回值类型泛型
-	 * @param source 源对象
-	 * @param target 目标对象
-	 * @return 目标对象
-	 */
-	public static <B> B copyProperties(Object source, B target) {
-		BeanUtils.copyProperties(source, target);
-		return target;
-	}
-
 
 	/**
 	 * 复制属性
@@ -42,34 +27,15 @@ public class LKBeanUtils {
 	 * @param <B> 返回值类型泛型
 	 * @param source 源对象
 	 * @param targetClass 目标对象类型
-	 * @return 目标对象
-	 */
-	public static <B> B copyProperties(Object source, Class<B> targetClass) {
-		try {
-			B target = targetClass.newInstance();
-			BeanUtils.copyProperties(source, target);
-			return target;
-		} catch (InstantiationException | IllegalAccessException e) {
-			throw new LKFrameworkException(e);
-		}
-	}
-
-
-	/**
-	 * 复制属性
-	 * @param <B> 返回值类型泛型
-	 * @param source 源对象
-	 * @param targetClass 目标对象类型
 	 * @param ignoreProperties 不复制的属性
 	 * @return 目标对象
 	 */
 	public static <B> B copyProperties(Object source, Class<B> targetClass, String... ignoreProperties) {
 		try {
-			B target = targetClass.newInstance();
-			BeanUtils.copyProperties(source, target, ignoreProperties);
-			return target;
+			return copyProperties(source, targetClass.newInstance(), ignoreProperties);
 		} catch (InstantiationException | IllegalAccessException e) {
-			throw new LKFrameworkException(e);
+			e.printStackTrace();// ignore this
+			return null;
 		}
 	}
 
