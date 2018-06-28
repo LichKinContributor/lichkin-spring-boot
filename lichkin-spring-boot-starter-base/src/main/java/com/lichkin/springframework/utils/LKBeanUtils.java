70ddffea06d2ebeaf400b2816d1dd3165aab15cf
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-06-28 22:15:01 +0800

    对象工具类

--- lichkin-spring-boot-starter-base/src/main/java/com/lichkin/springframework/utils/LKBeanUtils.java
+++ lichkin-spring-boot-starter-base/src/main/java/com/lichkin/springframework/utils/LKBeanUtils.java
@@ -2,6 +2,9 @@ package com.lichkin.springframework.utils;
 
 import org.springframework.beans.BeanUtils;
 
+import com.lichkin.framework.defines.enums.impl.LKErrorCodesEnum;
+import com.lichkin.framework.defines.exceptions.LKRuntimeException;
+
 /**
  * Bean工具类
  * @author SuZhou LichKin Information Technology Co., Ltd.
@@ -23,19 +26,18 @@ public class LKBeanUtils {
 
 
 	/**
-	 * 复制属性
-	 * @param <B> 返回值类型泛型
+	 * 创建对象
+	 * @param <B> 目标对象类型泛型
 	 * @param source 源对象
 	 * @param targetClass 目标对象类型
-	 * @param ignoreProperties 不复制的属性
+	 * @param ignoreProperties 忽略复制的字段名
 	 * @return 目标对象
 	 */
-	public static <B> B copyProperties(Object source, Class<B> targetClass, String... ignoreProperties) {
+	public static <B> B newInstance(final Object source, final Class<B> targetClass, final String... ignoreProperties) {
 		try {
 			return copyProperties(source, targetClass.newInstance(), ignoreProperties);
 		} catch (InstantiationException | IllegalAccessException e) {
-			e.printStackTrace();// ignore this
-			return null;
+			throw new LKRuntimeException(LKErrorCodesEnum.INTERNAL_SERVER_ERROR, e);
 		}
 	}
 
