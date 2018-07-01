3087c0f17422cbddaa683818a66669dae4a657bd
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-07-01 23:56:59 +0800

    完善validation

--- lichkin-spring-boot-starter-web-api/src/main/java/com/lichkin/springframework/controllers/LKApiController.java
+++ lichkin-spring-boot-starter-web-api/src/main/java/com/lichkin/springframework/controllers/LKApiController.java
@@ -9,10 +9,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.lichkin.framework.beans.LKRequestInterface;
 import com.lichkin.framework.beans.impl.LKResponseBean;
 import com.lichkin.framework.defines.LKFrameworkStatics;
+import com.lichkin.framework.defines.enums.impl.LKDateTimeTypeEnum;
 import com.lichkin.framework.defines.enums.impl.LKErrorCodesEnum;
+import com.lichkin.framework.defines.enums.impl.LKRangeTypeEnum;
 import com.lichkin.framework.defines.exceptions.LKException;
 import com.lichkin.framework.defines.exceptions.LKRuntimeException;
+import com.lichkin.framework.utils.LKDateTimeUtils;
+import com.lichkin.framework.utils.LKRandomUtils;
 import com.lichkin.framework.web.annotations.LKController4Api;
+import com.lichkin.framework.web.annotations.NotNeedCheckCompId;
+import com.lichkin.framework.web.annotations.NotNeedCheckToken;
 import com.lichkin.springframework.services.LKApiService;
 import com.lichkin.springframework.web.utils.LKRequestUtils;
 
@@ -33,22 +39,32 @@ public abstract class LKApiController<I extends LKRequestInterface, O> extends L
 	public LKResponseBean<O> invoke(@Valid @RequestBody I in) throws LKException {
 		in.setLocale(LKRequestUtils.getLocale(request).toString());
 
-		if (needCheckToken() && StringUtils.isBlank(in.getToken())) {
-			throw new LKRuntimeException(LKErrorCodesEnum.PARAM_ERROR);
-		}
-
-		// 脚本调用时compId在session中处理
 		if (in.getAppKey().startsWith("com.lichkin.app.javascript.")) {
 			// TODO 从Session中取
+			// 脚本调用时token在session中处理
+			in.setToken(LKDateTimeUtils.now(LKDateTimeTypeEnum.TIMESTAMP_MIN) + LKRandomUtils.create(47, LKRangeTypeEnum.NUMBER_AND_LETTER_FULL));
+
+			// TODO 从Session中取
+			// 脚本调用时compId在session中处理
 			in.setCompId(LKFrameworkStatics.ROOT);
 		} else {
+			// 客户端调用时，如果传入了token使用传入的值。
+			if (StringUtils.isBlank(in.getToken())) {
+				// 没有传入值，则判断是否需要校验，需要校验则抛异常。不需要校验则取默认值。
+				if (in.getClass().getAnnotation(NotNeedCheckToken.class) == null) {
+					throw new LKRuntimeException(LKErrorCodesEnum.PARAM_ERROR);
+				} else {
+					in.setToken("");
+				}
+			}
+
 			// 客户端调用时，如果传入了compId使用传入的值。
 			if (StringUtils.isBlank(in.getCompId())) {
 				// 没有传入值，则判断是否需要校验，需要校验则抛异常。不需要校验则取默认值。
-				if (needCheckCompId()) {
+				if (in.getClass().getAnnotation(NotNeedCheckCompId.class) == null) {
 					throw new LKRuntimeException(LKErrorCodesEnum.PARAM_ERROR);
 				} else {
-					in.setCompId(LKFrameworkStatics.ROOT);
+					in.setCompId("");
 				}
 			}
 		}
@@ -62,20 +78,6 @@ public abstract class LKApiController<I extends LKRequestInterface, O> extends L
 	 * @return 服务类
 	 */
 	protected abstract LKApiService<I, O> getService();
-
-
-	/**
-	 * 需要验证令牌
-	 * @return true:需要验证;false:不需要验证.
-	 */
-	protected abstract boolean needCheckToken();
-
-
-	/**
-	 * 需要公司ID
-	 * @return true:需要验证;false:不需要验证.
-	 */
-	protected abstract boolean needCheckCompId();
 
 
 	/**
