d91619c1b0f58f765ece9a5065f8139c7c90d6ec
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-07-11 22:04:29 +0800

    调整服务类业务定义

--- lichkin-spring-boot-starter-database/src/main/java/com/lichkin/springframework/services/LKLoginService.java
+++ lichkin-spring-boot-starter-database/src/main/java/com/lichkin/springframework/services/LKLoginService.java
@@ -6,29 +6,13 @@ import com.lichkin.framework.defines.entities.I_Login;
  * 登录业务服务类
  * @author SuZhou LichKin Information Technology Co., Ltd.
  */
-public abstract class LKLoginService extends LKDBService {
+public interface LKLoginService {
 
 	/**
 	 * 按照令牌获取登录信息
 	 * @param token 令牌
 	 * @return 登录信息
 	 */
-	public abstract I_Login findUserLoginByToken(String token);
-
-
-	/**
-	 * 按照登录名获取登录信息
-	 * @param loginName 登录名/手机号码/身份证号/邮箱
-	 * @return 登录信息
-	 */
-	public abstract I_Login findUserLoginByLoginName(String loginName);
-
-
-	/**
-	 * 按照手机号码获取登录信息
-	 * @param cellphone 手机号码
-	 * @return 登录信息
-	 */
-	public abstract I_Login findUserLoginByCellphone(String cellphone);
+	public I_Login findUserLoginByToken(String token);
 
 }
