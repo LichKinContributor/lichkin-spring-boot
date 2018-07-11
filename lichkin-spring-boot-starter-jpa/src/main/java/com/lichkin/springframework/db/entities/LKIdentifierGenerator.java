99485179d00031317c42e99e46b396290f5ae199
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-07-11 22:03:57 +0800

    工具类抽取

--- lichkin-spring-boot-starter-jpa/src/main/java/com/lichkin/springframework/db/entities/LKIdentifierGenerator.java
+++ lichkin-spring-boot-starter-jpa/src/main/java/com/lichkin/springframework/db/entities/LKIdentifierGenerator.java
@@ -6,8 +6,6 @@ import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
 
-import com.lichkin.framework.defines.enums.impl.LKRangeTypeEnum;
-import com.lichkin.framework.utils.LKDateTimeUtils;
 import com.lichkin.framework.utils.LKRandomUtils;
 
 /**
@@ -18,7 +16,7 @@ public class LKIdentifierGenerator implements IdentifierGenerator {
 
 	@Override
 	public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
-		return LKDateTimeUtils.now() + LKRandomUtils.create(47, LKRangeTypeEnum.NUMBER_AND_LETTER_FULL);
+		return LKRandomUtils.create64();
 	}
 
 }
