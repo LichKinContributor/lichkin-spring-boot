444455115fe48e9a47166611879e170babc1d6d4
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-10-11 20:32:08 +0800

    LKApiBusDeleteService完善

--- lichkin-spring-boot-starter-service-api/src/main/java/com/lichkin/springframework/services/LKApiBusDeleteService.java
+++ lichkin-spring-boot-starter-service-api/src/main/java/com/lichkin/springframework/services/LKApiBusDeleteService.java
@@ -1,10 +1,13 @@
 package com.lichkin.springframework.services;
 
-import static com.lichkin.framework.defines.LKFrameworkStatics.SPLITOR;
+import java.util.List;
 
 import org.springframework.transaction.annotation.Transactional;
 
+import com.lichkin.framework.db.beans.QuerySQL;
 import com.lichkin.framework.defines.entities.I_ID;
+import com.lichkin.framework.defines.entities.I_UsingStatus;
+import com.lichkin.framework.defines.enums.impl.LKUsingStatusEnum;
 import com.lichkin.framework.defines.exceptions.LKException;
 
 /**
@@ -29,18 +32,49 @@ public abstract class LKApiBusDeleteService<SI extends I_ID, E extends I_ID> ext
 
 	@Transactional
 	@Override
-	public void handle(SI in) throws LKException {
-		String id = in.getId();
-		clearSubTables(id.contains(SPLITOR) ? id.split(SPLITOR) : new String[] { id });
-		dao.deleteOneOrMoreById(classE, id);
+	public void handle(SI sin) throws LKException {
+		QuerySQL sql = new QuerySQL(false, classE);
+		sql.in(getIdColumnResId(), sin.getId());
+		List<E> listEntity = dao.getList(sql, classE);
+		if (realDelete(sin)) {
+			for (E entity : listEntity) {
+				beforeDelete(sin, entity, entity.getId());
+			}
+			dao.removeList(listEntity);
+		} else {
+			for (E entity : listEntity) {
+				((I_UsingStatus) entity).setUsingStatus(LKUsingStatusEnum.DEPRECATED);
+				beforeDelete(sin, entity, entity.getId());
+			}
+			dao.mergeList(listEntity);
+		}
 	}
 
 
 	/**
-	 * 删除子表
-	 * @param ids 主表IDs
+	 * 获取主键对应的列资源ID
+	 * @return 主键对应的列资源ID
 	 */
-	protected void clearSubTables(String[] ids) {
+	protected abstract int getIdColumnResId();
+
+
+	/**
+	 * 删除数据前操作
+	 * @param sin 入参
+	 * @param entity 待删除数据
+	 * @param id 主键
+	 */
+	protected void beforeDelete(SI sin, E entity, String id) {
+	}
+
+
+	/**
+	 * 是否实际删除数据
+	 * @param sin 入参
+	 * @return true:实际删除;false:逻辑删除（需主表实体类实现）.
+	 */
+	protected boolean realDelete(SI sin) {
+		return true;
 	}
 
 }
