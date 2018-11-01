b5dc2ea4a881aeb76ff3b59911ee771aad0772bc
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-11-01 21:49:11 +0800

    LKApiServiceImpl增加常用方法getLocale/getCompId

--- lichkin-spring-boot-starter-service-api/src/main/java/com/lichkin/springframework/services/LKApiServiceImpl.java
+++ lichkin-spring-boot-starter-service-api/src/main/java/com/lichkin/springframework/services/LKApiServiceImpl.java
@@ -3,6 +3,10 @@ package com.lichkin.springframework.services;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
+import org.apache.commons.lang3.StringUtils;
+
+import com.lichkin.framework.defines.LKFrameworkStatics;
+
 /**
  * 接口服务类定义
  * @param <SI> 服务类入参类型
@@ -27,6 +31,28 @@ public abstract class LKApiServiceImpl<SI, SO> extends LKDBService {
 	public LKApiServiceImpl() {
 		super();
 		types = ((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments();
+	}
+
+
+	/**
+	 * 获取国际化
+	 * @param locale 国际化
+	 * @param busLocale 入参中的国际化
+	 * @return 国际化
+	 */
+	protected String getLocale(String locale, String busLocale) {
+		return StringUtils.isNotBlank(busLocale) ? busLocale : locale;
+	}
+
+
+	/**
+	 * 获取公司ID
+	 * @param compId 公司ID
+	 * @param busCompId 入参中的公司ID
+	 * @return 公司ID
+	 */
+	protected String getCompId(String compId, String busCompId) {
+		return LKFrameworkStatics.LichKin.equals(compId) && StringUtils.isNotBlank(busCompId) ? busCompId : compId;
 	}
 
 }
