d089ed3b4bc1ad2e13c7dc0773fee4f0f1440950
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-11-15 17:14:00 +0800

    增加基于数据库的动态任务服务类

---
+++ lichkin-spring-boot-starter-quartz-database/src/main/java/com/lichkin/framework/springboot/services/LKBaseDBDynamicJobService.java
@@ -0,0 +1,26 @@
+package com.lichkin.framework.springboot.services;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import lombok.Getter;
+
+/**
+ * 通用基本任务服务类
+ * @author SuZhou LichKin Information Technology Co., Ltd.
+ */
+@Getter
+public abstract class LKBaseDBDynamicJobService extends LKBaseTaskDBService implements Job {
+
+	/** 定时任务执行环境上下文 */
+	public JobExecutionContext context;
+
+
+	@Override
+	public void execute(final JobExecutionContext context) throws JobExecutionException {
+		this.context = context;
+		executeTask();
+	}
+
+}
