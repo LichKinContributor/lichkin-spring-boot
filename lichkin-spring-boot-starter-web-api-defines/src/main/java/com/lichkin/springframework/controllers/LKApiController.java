b7c6f02773ce29105b42272daa7d679b09dbe9a1
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-11-17 15:19:57 +0800

    LKApiController抽取定义，交由业务包实现。

---
+++ lichkin-spring-boot-starter-web-api-defines/src/main/java/com/lichkin/springframework/controllers/LKApiController.java
@@ -0,0 +1,74 @@
+package com.lichkin.springframework.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.lichkin.framework.beans.impl.LKRequestBean;
+import com.lichkin.framework.beans.impl.LKResponseBean;
+import com.lichkin.framework.defines.enums.impl.LKOperTypeEnum;
+import com.lichkin.framework.defines.exceptions.LKException;
+
+/**
+ * API数据请求控制器类定义
+ * @param <CI> 控制器类入参类型
+ * @param <CO> 控制器类出参类型
+ * @author SuZhou LichKin Information Technology Co., Ltd.
+ */
+public abstract class LKApiController<CI extends LKRequestBean, CO> extends LKController {
+
+	/**
+	 * 请求处理方法
+	 * @deprecated API必有方法，不可重写。
+	 * @param cin 控制器类入参
+	 * @return 控制器类出参
+	 * @throws LKException 业务处理失败但不希望已处理数据回滚时抛出异常
+	 */
+	@Deprecated
+	public LKResponseBean<CO> invoke(@Valid @RequestBody CI cin) throws LKException {
+		return null;
+	}
+
+
+	/**
+	 * 请求处理方法
+	 * @param cin 控制器类入参
+	 * @param locale 国际化
+	 * @param compId 公司ID
+	 * @param loginId 登录ID
+	 * @return 控制器类出参
+	 * @throws LKException 业务处理失败但不希望已处理数据回滚时抛出异常
+	 */
+	abstract CO handleInvoke(@Valid CI cin, String locale, String compId, String loginId) throws LKException;
+
+
+	/**
+	 * 是否记录日志
+	 * @param cin 控制器类入参
+	 * @return true:记录日志;false:不记录日志;
+	 */
+	protected boolean saveLog(CI cin) {
+		return true;
+	}
+
+
+	/**
+	 * 获取操作类型
+	 * @param cin 控制器类入参
+	 * @return 操作类型
+	 */
+	protected LKOperTypeEnum getOperType(@Valid CI cin) {
+		return null;
+	}
+
+
+	/**
+	 * 获取业务操作类型
+	 * @param cin 控制器类入参
+	 * @return 业务操作类型
+	 */
+	protected String getBusType(CI cin) {
+		return null;
+	}
+
+}
--- lichkin-spring-boot-starter-web-api/src/main/java/com/lichkin/springframework/controllers/ApiController.java
+++
@@ -1,6 +0,0 @@
-package com.lichkin.springframework.controllers;
-
-
-public interface ApiController {
-
-}
